e3bb3f2e0febbc081dfeb7c942019a94cf07291c
LolHens <pierrekisters@gmail.com>
2018-09-05 14:31:31 +0200

    fixed

--- src/main/scala/de/lolhens/scalajs/webjar/ScalaJSWebjarPlugin.scala
+++ src/main/scala/de/lolhens/scalajs/webjar/ScalaJSWebjarPlugin.scala
@@ -1,5 +1,6 @@
 package de.lolhens.scalajs.webjar
 
+import de.lolhens.scalajs.webjar.WebjarPlugin.autoImport._
 import org.scalajs.sbtplugin.ScalaJSPlugin.AutoImport._
 import sbt.Keys._
 import sbt._
