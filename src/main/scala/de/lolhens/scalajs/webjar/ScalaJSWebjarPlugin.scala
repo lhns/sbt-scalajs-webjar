96bddd1d479f1435352c33bd844f1ae07a82196f
LolHens <pierrekisters@gmail.com>
2018-09-13 15:12:13 +0200

    cleanup

--- src/main/scala/de/lolhens/scalajs/webjar/ScalaJSWebjarPlugin.scala
+++ src/main/scala/de/lolhens/scalajs/webjar/ScalaJSWebjarPlugin.scala
@@ -7,14 +7,6 @@ import sbt.Keys._
 import sbt._
 
 object ScalaJSWebjarPlugin extends AutoPlugin {
-
-  object autoImport {
-    lazy val isDevMode = taskKey[Boolean]("Whether the app runs in development mode")
-    lazy val scalaJS = taskKey[Seq[File]]("ScalaJS output files")
-  }
-
-  import autoImport._
-
   override def requires: Plugins = ScalaJSPlugin && WebjarPlugin
 
   override lazy val projectSettings = Seq(
@@ -23,35 +15,11 @@ object ScalaJSWebjarPlugin extends AutoPlugin {
     skip in packageJSDependencies := false,
     Compile / packageJSDependencies / crossTarget := (Compile / resourceManaged).value,
 
-    fastOptJS / scalaJS := Seq(
-      (Compile / fastOptJS).value.data,
-      (Compile / fastOptJS).value.map(file => new File(file.toString + ".map")).data,
-      (Compile / packageJSDependencies).value
-    ),
-
-    fullOptJS / scalaJS := Seq(
+    webjarMappings ++= Seq(
       (Compile / fullOptJS).value.data,
       (Compile / fullOptJS).value.map(file => new File(file.toString + ".map")).data,
       (Compile / packageMinifiedJSDependencies).value
-    ),
-
-    isDevMode := {
-      val devCommands = Seq("run", "compile", "re-start", "reStart", "runAll")
-      val executedCommandKey =
-        state.value.history.currentOption
-          .flatMap(_.commandLine.takeWhile(c => !c.isWhitespace).split(Array('/', ':')).lastOption)
-          .getOrElse("")
-      devCommands.contains(executedCommandKey)
-    },
-
-    scalaJS := Def.taskDyn {
-      if (isDevMode.value)
-        fastOptJS / scalaJS
-      else
-        fullOptJS / scalaJS
-    }.value,
-
-    webjarMappings ++= scalaJS.value.map { file =>
+    ).map { file =>
       file -> file.name
     }
   )
