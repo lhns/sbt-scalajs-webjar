5f973feb5988855e7520c8d41ba8d83f4ae155c2
Pierre Kisters <pierrekisters@gmail.com>
2023-04-24 23:49:18 +0200

    fix source maps in sbt-scalajs-bundler-webjar

--- sbt-scalajs-bundler-webjar/src/main/scala/de/lolhens/sbt/scalajs/webjar/ScalaJSBundlerWebjarPlugin.scala
+++ sbt-scalajs-bundler-webjar/src/main/scala/de/lolhens/sbt/scalajs/webjar/ScalaJSBundlerWebjarPlugin.scala
@@ -2,6 +2,7 @@ package de.lolhens.sbt.scalajs.webjar
 
 import de.lolhens.sbt.scalajs.webjar.ScalaJSWebjarPlugin.stagedOptJS
 import de.lolhens.sbt.scalajs.webjar.WebjarPlugin.autoImport._
+import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 import sbt.Keys._
 import sbt._
 import scalajsbundler.sbtplugin.ScalaJSBundlerPlugin
@@ -15,13 +16,24 @@ object ScalaJSBundlerWebjarPlugin extends AutoPlugin {
 
     Compile / webjarArtifacts := {
       val attributedFiles = stagedOptJS(Compile / _ / webpack).value
+      val emitSourceMaps = {
+        stagedOptJS(e => Def.optional(e / webpackEmitSourceMaps)(identity)).value
+          .getOrElse(stagedOptJS(_ / scalaJSLinkerConfig).value.sourceMap)
+      }
       val target = (Compile / webjarArtifacts / crossTarget).value
+
+      def sourceMapFor(file: File): Option[File] =
+        if (emitSourceMaps && file.getName.toLowerCase.endsWith(".js"))
+          Some(new File(file.toString + ".map"))
+        else
+          None
 
       val artifacts = Seq(
         attributedFiles.find(_.metadata.get(BundlerFileTypeAttr).exists(_ == BundlerFileType.ApplicationBundle)).map(_.data),
         attributedFiles.find(_.metadata.get(BundlerFileTypeAttr).exists(_ == BundlerFileType.Asset)).map(_.data),
       )
         .flatMap(_.toList)
+        .flatMap(file => file +: sourceMapFor(file).toList)
         .map(file => (file, target / file.name))
 
       IO.copy(artifacts)
